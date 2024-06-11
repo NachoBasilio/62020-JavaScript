e7d69ef16c639615d623c50c5330b8c06e79e278
IgnaDev <arcortoon@gmail.com>
2024-06-10 22:39:06 -0300

    Clase terminada

---
+++ Semana 1/Clase/scripts/main.js
@@ -0,0 +1,50 @@
+let nombre
+let casado = true
+let edad = NaN
+
+let apellido = "Solis"
+
+const url = "www.google.com"
+const pi = 3.14
+
+
+nombre = "Elian"
+
+// console.log(nombre)
+
+nombre = "Alan"
+
+
+
+// console.log(nombre)
+
+// console.log(casado)
+
+console.log(34 + 1)// 341
+// console.log("34" - 1)// 33
+// console.log("34" / 2)
+// console.log("34" * 2)
+
+
+console.log(nombre + " " + apellido)
+
+//alert("Bienvenido")
+
+//let confirmacion = confirm("¿Queres facturar eso?")
+
+//console.log(confirmacion)
+
+// nombre = prompt("¿Cual es tu nombre?")
+
+// console.log(nombre)
+
+// alert("Hola " + nombre + " ¿Como estas?")
+
+
+edad = prompt("Cuantos años tenes?")
+
+edad = parseInt(edad)
+
+let edadDentroDe20Años = edad + 20
+
+alert("Dentro de 20 años tendrias: " + edadDentroDe20Años )
