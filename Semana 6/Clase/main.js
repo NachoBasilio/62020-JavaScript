95a38b1a607007c6986620519db8fc94787d8d2b
IgnaDev <arcortoon@gmail.com>
2024-07-22 15:56:45 -0300

    Agregando material necesario

---
+++ Semana 6/Clase/main.js
@@ -0,0 +1,115 @@
+const productosLibreria = [
+    {
+        id: 1,
+        nombre: 'Cuaderno',
+        precio: 2.50,
+        categoria: 'Papelería',
+        stock: 100
+    },
+    {
+        id: 2,
+        nombre: 'Lápiz',
+        precio: 0.50,
+        categoria: 'Papelería',
+        stock: 200
+    },
+    {
+        id: 3,
+        nombre: 'Bolígrafo',
+        precio: 1.20,
+        categoria: 'Papelería',
+        stock: 150
+    },
+    {
+        id: 4,
+        nombre: 'Borrador',
+        precio: 0.30,
+        categoria: 'Papelería',
+        stock: 300
+    },
+    {
+        id: 5,
+        nombre: 'Resaltador',
+        precio: 1.00,
+        categoria: 'Papelería',
+        stock: 75
+    },
+    {
+        id: 6,
+        nombre: 'Mochila',
+        precio: 20.00,
+        categoria: 'Accesorios',
+        stock: 50
+    },
+    {
+        id: 7,
+        nombre: 'Regla',
+        precio: 0.80,
+        categoria: 'Papelería',
+        stock: 120
+    },
+    {
+        id: 8,
+        nombre: 'Tijeras',
+        precio: 2.00,
+        categoria: 'Papelería',
+        stock: 90
+    },
+    {
+        id: 9,
+        nombre: 'Pegamento',
+        precio: 1.50,
+        categoria: 'Papelería',
+        stock: 60
+    },
+    {
+        id: 10,
+        nombre: 'Calculadora',
+        precio: 15.00,
+        categoria: 'Electrónica',
+        stock: 30
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numeros = [ 40, 1, 5, 200 ];
+
+// numeros.sort((a, b) => a - b);  // [ 1, 5, 40, 200 ]
+
+// numeros.sort((a, b) => b - a);  // [ 200, 40, 5, 1 ]
